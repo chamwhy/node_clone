e99d494d4af142a860068a6a26c7ecd2973ea96c
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-06-14 18:10:06 +0900

    add cors helmet xss viewer

--- config/express.js
+++ config/express.js
@@ -1,7 +1,20 @@
+"use strict";
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
+  app.use(helmet());
+  app.use(cors());
+  app.use(helmet.xssFilter());
+  app.get("/", (req, res) => {});
 
-    });
+  app.use(express.static(config.root + "/public"));
+
+  app.set("views", config.root + "/app/views");
+  app.set("view engine", "ejs");
+
+  app.use(bodyParser.json());
 };
